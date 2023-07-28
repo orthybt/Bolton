6aea1dda4dbbeaa3a90e984720958c02f5b17977
orthybt <orthybt@gmail.com>
2023-07-28 02:30:18 +0200

    a

--- src/main/java/com/example/bolton/BoltonApplication.java
+++ src/main/java/com/example/bolton/BoltonApplication.java
@@ -8,12 +8,41 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class BoltonApplication extends Application {
+	//names(thus locations) of fxml files
+	private String BoltonViewName = "BoltonView";
+
+	/**start is some superclass/interface method that must be implemented by
+	 * you, its used in code modularity and code reuse. Its a contract that
+	 * one needs and should follow, so maybe one day someone else would be
+	 * able to use your code. It also introduces good ways of software design.
+	 *
+	 * @param stage the primary stage for this application, onto which
+	 * the application scene can be set. A JFrame, the boundaries between
+	 *                 Windows and javaFX. Its an accesor object between
+	 *                 different plaforms, it grants access to Windows
+	 *                 commands, to the JavaFX language.
+	 * Applications may create other stages, if needed, but they will not be
+	 * primary stages.
+	 *              The origin of the object Stage, where and  how is
+	 *              instantiated it should not concern us,the clients, it's a
+	 *              low level absolutely not needed to know language, and the
+	 *              guys behind fxml were kind to improve the java language,
+	 *              big hooray and bravo , and also thank you very much. Love
+	 *              <3 This is object encapsulation in a nutshell.
+	 * @throws IOException
+	 */
 	@Override
 	public void start(Stage stage) throws IOException {
+		//this object is responsible for the interface between javaFX and a
+		// fxml file, basically it translates BoltonView.fxml to java fx code
 		FXMLLoader fxmlLoader =
-				new FXMLLoader(BoltonApplication.class.getResource("hello-view" +
+				//.class returns the current location of Bolton.class
+				new FXMLLoader(BoltonApplication.class.getResource(
+						"BoltonView" +
 						".fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+		//JPanel , mainPanel
+		Scene scene = new Scene(fxmlLoader.load());
+		//gui.set the title, JFrame.setVisible(true)
 		stage.setTitle("Hello!"); stage.setScene(scene); stage.show();
 	}
 
--- src/main/java/com/example/bolton/BoltonController.java
+++ src/main/java/com/example/bolton/BoltonController.java
@@ -2,13 +2,59 @@ package com.example.bolton;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class BoltonController {
 	@FXML
-	private Label welcomeText;
+	private Label header;
+	@FXML
+	private TextField sumMax;
+	@FXML
+	private TextField sumMand;
+	@FXML
+	private TextField myRatio;
+	@FXML
+	private TextField toothDeficiencyLocation;
+	@FXML
+	private TextField toothDeficiencyAmmount;
+
+
+
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("Welcome to JavaFX Application!");
+	protected void onCalculateButtonClick() {
+		//Grab the values
+		Double sumMaxTemp;
+		Double sumMandTemp;
+		Double baseRatioTemp = 0.913;
+		Double baseRatioTempLowerLimit = 0.893;
+		Double baseRatioTempUpperLimit = 0.932;
+
+		sumMaxTemp = Double.valueOf(sumMax.getText());
+		sumMandTemp = Double.valueOf(sumMand.getText());
+		Double myRatioTemp = sumMandTemp/sumMaxTemp;
+		myRatio.setText(String.format("%.2f",myRatioTemp));
+
+		if (myRatioTemp >= baseRatioTempLowerLimit && myRatioTemp <= baseRatioTempUpperLimit) {
+			toothDeficiencyLocation.setText("Tooth size discrepancy within " +
+					"normal limits");
+			Double value = Math.abs((myRatioTemp - baseRatioTemp)*100);
+			toothDeficiencyAmmount.setText(value.toString());
+		}
+		if (myRatioTemp > baseRatioTempUpperLimit) {
+			//if the value of sumMand increases so does the my ratio, the
+			// rise of ratio is the rise of mandibular tooth material
+			toothDeficiencyLocation.setText("Maxillary Deficiency");
+			Double value = Math.abs((myRatioTemp - baseRatioTemp)*100);
+			toothDeficiencyAmmount.setText(String.format("%.2f", value));
+			System.out.println("HERE");
+
+		}
+		if (myRatioTemp < baseRatioTempLowerLimit) {
+			toothDeficiencyLocation.setText("Mandibular Deficiency");
+			Double value = Math.abs((myRatioTemp - baseRatioTemp)*100);
+
+			toothDeficiencyAmmount.setText(String.format("%.2f", value));
+		}
 	}
 }
